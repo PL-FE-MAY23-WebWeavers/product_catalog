7dd957ae6fa1f22610e9c703999b4aa9ce8d75a2
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-10-31 14:01:27 +0100

    add burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,69 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Logo } from '../Logo/Logo';
 import cn from 'classnames';
 import './header.scss';
 
-export const Header = () => (
-  <header className='header'>
-    <div className="header__top" id="header__top">
-      <Logo/>
-    </div>
-    <nav className='header__nav'>
-      <NavLink
-        to='/'
-        className={({ isActive }) =>
-          cn('header__link', {
-            'is-active': isActive,
-          })
-        }>
-          Home
-      </NavLink>
-      <NavLink
-        to='/phones'
-        className={({ isActive }) =>
-          cn('header__link', {
-            'is-active': isActive,
-          })
-        }>
-          Phones
-      </NavLink>
-      <NavLink
-        to='/tablets'
-        className={({ isActive }) =>
-          cn('header__link', {
-            'is-active': isActive,
-          })
-        }>
-          Tablets
-      </NavLink>
-      <NavLink
-        to='/accessories'
-        className={({ isActive }) =>
-          cn('header__link', {
-            'is-active': isActive,
-          })
-        }>
-          Accessories
-      </NavLink>
-    </nav>
-    <div className='header__sub-nav'>
-      <NavLink
-        to='/favourites'
-        className={({ isActive }) =>
-          cn('header__ico-link header__ico-link--favs', {
-            'is-active': isActive,
-          })
-        }>
-      </NavLink>
-      <NavLink
-        to='/cart'
-        className={({ isActive }) =>
-          cn('header__ico-link header__ico-link--cart', {
-            'is-active': isActive,
-          })
-        }>
-      </NavLink>
-    </div>
-  </header>
-);
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  return (
+    <header
+      className={cn('header', {
+        'header--menu-open': isMenuOpen
+      })}
+    >
+      <div className="header__top" id="header__top">
+        <Logo/>
+        <button
+          className={cn('header__menu', {
+            'header__menu--active': isMenuOpen
+          })}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <div className='header__menu-icon'>
+            <div className="header__menu-bar header__menu-bar--top"></div>
+            <div className="header__menu-bar header__menu-bar--mid"></div>
+            <div className="header__menu-bar header__menu-bar--bottom"></div>
+          </div>
+        </button>
+      </div>
+      <nav className='header__nav'>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            cn('header__link', {
+              'header__link--active': isActive,
+            })
+          }>
+            Home
+        </NavLink>
+        <NavLink
+          to='/phones'
+          className={({ isActive }) =>
+            cn('header__link', {
+              'header__link--active': isActive,
+            })
+          }>
+            Phones
+        </NavLink>
+        <NavLink
+          to='/tablets'
+          className={({ isActive }) =>
+            cn('header__link', {
+              'header__link--active': isActive,
+            })
+          }>
+            Tablets
+        </NavLink>
+        <NavLink
+          to='/accessories'
+          className={({ isActive }) =>
+            cn('header__link', {
+              'header__link--active': isActive,
+            })
+          }>
+            Accessories
+        </NavLink>
+      </nav>
+      <div className='header__sub-nav'>
+        <NavLink
+          to='/favourites'
+          className={({ isActive }) =>
+            cn('header__ico-link header__ico-link--favs', {
+              'header__link--active': isActive,
+            })
+          }>
+        </NavLink>
+        <NavLink
+          to='/cart'
+          className={({ isActive }) =>
+            cn('header__ico-link header__ico-link--cart', {
+              'header__link--active': isActive,
+            })
+          }>
+        </NavLink>
+      </div>
+    </header>
+  );
+};
