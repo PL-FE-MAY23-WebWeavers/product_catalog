d114b872b402299bfcbd03920fe7e574c206badb
Nightycake <michalmarjak@gmail.com>
2023-11-02 14:28:27 +0100

    add reusable fetching function

---
+++ src/services/productsService.ts
@@ -0,0 +1,28 @@
+const URL_BASE = 'https://webweavers.onrender.com';
+const ENDPOINT = '/products';
+
+interface APIResponse<T> {
+  status: number;
+  data: T;
+}
+
+export async function fetchDataFromAPI<T>(
+  params: Record<string, string | number> = {}
+): Promise<APIResponse<T>> {
+  try {
+    const url = new URL(ENDPOINT, URL_BASE);
+    const searchParams = new URLSearchParams();
+    for (const key in params) {
+      searchParams.append(key, String(params[key]));
+    }
+
+    url.search = searchParams.toString();
+    const response = await fetch(url.toString());
+    const responseData: APIResponse<T> = await response.json();
+
+    return responseData;
+  } catch (error) {
+    console.error('Error occurred while fetching data:', error);
+    throw error;
+  }
+}
