f06f1f513f63791eff901690e7b9330888242e78
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-11-03 19:58:42 +0100

    add Slider to HomePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Slider } from '../components/Slider/Slider';
 
 export const Home = () => {
   return (
-    <div className='grid-global'>
-      <h1 className='homepage-item__h1'>
+    <div className='grid-container'>
+      <div className='grid-global'>
+        <h1 className='homepage-item__h1'>
       Welcome to Nice Gadgets store!
-      </h1>
+        </h1>
 
-      <div className='homepage-item__section'>
+        <div className='homepage-item__slider'>
+          <Slider />
+        </div>
+
+        <div className='homepage-item__section'>
+
+        </div>
+
+        <div className='homepage-item__section'>
+
+        </div>
 
       </div>
-
-      <div className='homepage-item__section'>
-
-      </div>
-
-      <div className='homepage-item__section'>
-
-      </div>
-
     </div>
   );
 };
