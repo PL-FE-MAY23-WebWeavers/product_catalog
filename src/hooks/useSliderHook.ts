c595c5669abb02a2a67ee5be7df77b60f0d9b9b9
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-11-06 16:52:03 +0100

    add working slider

---
+++ src/hooks/useSliderHook.ts
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+
+type useSliderHookProps = {
+images: string[],
+}
+
+export const useSliderHook = ({images}: useSliderHookProps ) => {
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 639);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 639);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    images.forEach(src => {
+      const img = new Image();
+      img.src = src;
+    });
+  }, [images]);
+
+  return {
+    isMobileView,
+  };
+};
