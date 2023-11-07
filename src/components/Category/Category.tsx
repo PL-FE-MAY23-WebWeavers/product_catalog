969a3b89cb39fb6bac4b39daa390d3ff9196b580
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-11-07 08:46:38 +0100

    add Category WIP

---
+++ src/components/Category/Category.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './category.scss';
+
+type CategoryProps = {
+categoryImg: string,
+category: string,
+index: number,
+}
+
+export const Category = ({categoryImg, category, index}: CategoryProps ) => {
+  return (
+    <div className='category'>
+      <img
+        src={categoryImg}
+        alt={category}
+        className={`category__img category__img--${index}`}/>
+      <h4>
+        {category}
+      </h4>
+      <p>95 models</p>
+    </div>
+  );
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
+import { Category } from '../components/Category/Category';
 
 export const Home = () => {
+  const url = 'https://webweavers.onrender.com/img/';
+  const imgs = [
+    'category-phones.png',
+    'category-tablets.png',
+    'category-accessories.png',
+  ];
+  const categories = [
+    'Mobile phones',
+    'Tablets',
+    'Accessories',
+  ];
+
   return (
     <div className='grid-global'>
       <h1 className='homepage-item__h1'>
       Welcome to Nice Gadgets store!
       </h1>
 
-      <div className='homepage-item__section'>
+      <section className='homepage-item__section'>
 
-      </div>
+      </section>
 
-      <div className='homepage-item__section'>
+      <section className='homepage-item__section'>
+        <h2>
+          Shop by category
+        </h2>
+        {imgs.map((img, index) => {
+          return (
+            <Category
+              key={index}
+              categoryImg={url + img}
+              category={categories[index]}
+              index={index}
+            />
+          );})
+        }
+      </section>
 
-      </div>
+      <section className='homepage-item__section'>
 
-      <div className='homepage-item__section'>
-
-      </div>
+      </section>
 
     </div>
   );
