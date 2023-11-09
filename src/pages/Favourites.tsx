cd388428ea4a574fdc6480adb876681a38b44e73
Nightycake <michalmarjak@gmail.com>
2023-11-09 12:08:19 +0100

    add fav counter and no phones text

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 
-import { Pagination } from '../components/Pagination/Pagination';
 import { usePhonesContext } from '../providers/PhonesProvider/PhonesProvider';
 import { Loader } from '../components/Loader/Loader';
 import { CardsLayout } from '../components/CardsLayout/CardsLayout';
 import { BreadCrumbs } from '../components/BreadCrumbs/BreadCrumbs';
 import { Wrapper } from '../components/utils/Wrapper/Wrapper';
 import './phones.scss';
+import { useProductCatalog } from '../context/ProductCatalogContext';
 
-export const Favourites = () => {
+export const Favourites: React.FC = () => {
   const { isLoading } = usePhonesContext();
+  const { favourites } = useProductCatalog();
 
   return (
     <section className="phones">
       <Wrapper>
         <BreadCrumbs />
         <h1 className="phones__h1">Favourites</h1>
+        <p className="phones__count p">{favourites.length} models</p>
+        {favourites.length === 0 && (
+          <h3 className="phones__h1">No phones have been added to favourites!</h3>
+        )}
         {isLoading && <Loader />}
         <div className="phones__grid grid-global">
           <CardsLayout />
-          {!isLoading && <Pagination />}
+          {!isLoading}
         </div>
       </Wrapper>
     </section>
